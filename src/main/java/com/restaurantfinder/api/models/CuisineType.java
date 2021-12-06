61aa93116333dffbfbc4ecaf6e22fd557d07616c
jFarrow02 <jack.dempsey.farrow@gmail.com>
2021-12-06 10:40:05 -0500

    adds collection to Document annotation for CuisineType

--- src/main/java/com/restaurantfinder/api/models/CuisineType.java
+++ src/main/java/com/restaurantfinder/api/models/CuisineType.java
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-@Document
+@Document(collection = "cuisine-types")
 public class CuisineType {
 
     @Id
